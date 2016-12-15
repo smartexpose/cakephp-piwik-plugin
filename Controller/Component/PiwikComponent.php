e72c4ec821194d1db949f3e7e4dbcab724bf7087
Eric Büttner <eric.buettner@tuffz.com>
2016-12-15 13:32:04 +0100

    add dispatchPiwikTrackerMethod()

--- Controller/Component/PiwikComponent.php
+++ Controller/Component/PiwikComponent.php
@@ -38,9 +38,36 @@ class PiwikComponent extends Component {
  */
 	public function __call($method, $params) {
 		if (method_exists($this->PiwikTracker, $method)) {
-			return $this->dispatchMethod(array($this->PiwikTracker, $method), $params);
+			return $this->dispatchPiwikTrackerMethod($method, $params);
 		}
 		trigger_error(__d('cake_dev', 'Method %1$s::%2$s does not exist', get_class($this), $method), E_USER_WARNING);
+	}
+
+/**
+ * Calls a method on PiwikTracker object with the given parameters. Provides an OO wrapper
+ * for `call_user_func_array`
+ *
+ * @param string $method Name of the method to call
+ * @param array $params Parameter list to use when calling $method
+ * @return mixed Returns the result of the method call
+ */
+	public function dispatchPiwikTrackerMethod($method, $params = array()) {
+		switch (count($params)) {
+			case 0:
+				return $this->PiwikTracker->{$method}();
+			case 1:
+				return $this->PiwikTracker->{$method}($params[0]);
+			case 2:
+				return $this->PiwikTracker->{$method}($params[0], $params[1]);
+			case 3:
+				return $this->PiwikTracker->{$method}($params[0], $params[1], $params[2]);
+			case 4:
+				return $this->PiwikTracker->{$method}($params[0], $params[1], $params[2], $params[3]);
+			case 5:
+				return $this->PiwikTracker->{$method}($params[0], $params[1], $params[2], $params[3], $params[4]);
+			default:
+				return call_user_func_array(array(&$this->PiwikTracker, $method), $params);
+		}
 	}
 
 }
